5158bfb1e018ad1652167339f9db017b38f6b388
rattalur <raviteja_attaluri@bmc.com>
2021-10-18 23:10:53 +0530

    Changes for reading string & parsing practice.

--- practice/main.go
+++ practice/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,5 +14,29 @@ func main() {
 	fmt.Print("Enter text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
+
+	reader2 := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter number: ")
+	input2, _ := reader2.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Print("Entered Number is : ")
+		fmt.Println(aFloat)
+		fmt.Printf("Entered Number format is : %T\n", aFloat)
+	}
+
+	anInt, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Print("Entered Number is : ")
+		fmt.Println(anInt)
+		fmt.Printf("Entered Number format is : %T\n", anInt)
+	}
+	var anInt2 string = strconv.FormatFloat(anInt, 'f', 5, 64)
+	fmt.Print("Integer is : ")
+	fmt.Println(anInt2)
 
 }
