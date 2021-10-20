421d8c8c68862aa8f5f76cd7af1a1b6e808bce37
rattalur <raviteja_attaluri@bmc.com>
2021-10-20 22:54:40 +0530

    Calculator task

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("A simple calculator")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter a number1:")
+	stg1, _ := reader.ReadString('\n')
+	fmt.Printf("Enter a number1:")
+	stg2, _ := reader.ReadString('\n')
+
+	number1, err := strconv.ParseFloat(strings.TrimSpace(stg1), 64)
+	number2, err2 := strconv.ParseFloat(strings.TrimSpace(stg2), 64)
+
+	if err != nil || err2 != nil {
+		panic("unable to parse number, please pass a valid format")
+	}
+
+	number3 := number1 + number2
+	fmt.Println("Total : ", number3)
+
 }
