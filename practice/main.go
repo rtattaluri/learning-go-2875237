5a26ee83ef612c00c6e32fce68130389fc90a77d
rattalur <raviteja_attaluri@bmc.com>
2021-10-20 23:07:46 +0530

    pointers.

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	p := &anInt
+	fmt.Println("pointer ", *p)
+	fmt.Println("value ", anInt)
+
+	anInt = 21
+	fmt.Println("reassignment pointer ", *p)
+	fmt.Println("reassignment value ", anInt)
+
+	aFloat := 4.2
+	p2 := &aFloat
+	fmt.Println("Float pointer ", *p2)
+	fmt.Println("Float value ", aFloat)
+
+	*p2 = *p2 / 2
+	fmt.Println("Float pointer ", *p2)
+	fmt.Println("Float value ", aFloat)
+
 }
