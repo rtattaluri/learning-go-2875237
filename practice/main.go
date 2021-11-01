d1d4e8ab3192b684008e0004114890288dbf5625
rattalur <raviteja_attaluri@bmc.com>
2021-11-01 15:20:42 +0530

    Changes for arrays

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	colors := [3]string{"Red", "Blue", "Green"}
+	fmt.Println("Colors :", colors)
+
+	var num [3]int
+
+	num[1] = 42
+	num[2] = 38
+	num[0] = 0
+	fmt.Println("Numbers: ", num)
+
+	fmt.Println("length of colors: ", len(colors))
+	fmt.Println("length of numbers: ", len(num))
+
 }
