2282925719ab19b1a2fb1376f7c526ee09938aa3
rattalur <raviteja_attaluri@bmc.com>
2021-11-01 16:02:11 +0530

    chnages for slices practice

--- practice/main.go
+++ practice/main.go
@@ -2,9 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	var colors = [3]string{"Red", "Green", "Blue"}
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+	fmt.Println(len(colors))
+
+	colors = append(colors, "Yellow")
+	fmt.Println(colors)
+	fmt.Println(len(colors))
+
+	colors = append(colors[1:len(colors)])
+	fmt.Println(colors)
+	fmt.Println(len(colors))
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+	fmt.Println(len(colors))
+
+	numbers := make([]int, 5)
+	fmt.Println(numbers)
+	fmt.Println(len(numbers))
+	numbers[0] = 101
+	numbers[1] = 72
+	numbers[2] = 32
+	numbers[3] = 96
+	numbers[4] = 54
+	fmt.Println(numbers)
+	fmt.Println(len(numbers))
+
+	numbers = append(numbers, 110)
+	fmt.Println(numbers)
+	fmt.Println(len(numbers))
+
+	sort.Ints(numbers)
+	fmt.Println(numbers)
+
 }
