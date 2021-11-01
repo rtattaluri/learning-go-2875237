f14eb5effd0226052db1f3e7fef9026c2a5c0461
rattalur <raviteja_attaluri@bmc.com>
2021-11-01 17:43:26 +0530

    switch stmt

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,28 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var result string
+	switch dow {
+	case 0:
+		result = "sunday"
+	case 1:
+		result = "monday"
+	case 2:
+		result = "tuesday"
+	case 3:
+		result = "wednesday"
+	case 4:
+		result = "thursday"
+	case 5:
+		result = "friday"
+	case 6:
+		result = "saturday"
+	case 7:
+		result = "sunday"
+	default:
+		result = "no other day left!!!"
+	}
+
+	fmt.Println(" Day of week: ", result)
 }
