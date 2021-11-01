722e83d272beca07285123998d457568d8ab80a5
rattalur <raviteja_attaluri@bmc.com>
2021-11-01 17:33:07 +0530

    chnages for if stmt

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	a := 43
+	var result string
+	if a < 0 {
+		result = "Less than zero"
+	} else if a == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
+	if a := -1; a < 0 {
+		result = "Less than zero"
+	} else if a == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
 }
