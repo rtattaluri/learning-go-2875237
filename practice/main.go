98ba61b09d39fafa988f2573da0d242b561617e9
rattalur <raviteja_attaluri@bmc.com>
2021-11-01 18:02:52 +0530

    chnages for loop , break, goto, continue

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,29 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	x := 0
+	for x < 25 {
+		x += 1
+		fmt.Println("Sum is ", x)
+		if x > 20 {
+			break
+		}
+		fmt.Println("final Sum is ", x)
+	}
+
+	fmt.Println("Program finished")
+
 }
