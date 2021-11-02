488463370dfcb85df6af1068ceccff725284a27e
rattalur <raviteja_attaluri@bmc.com>
2021-11-02 22:56:15 +0530

    chnages for methods practice

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,28 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Wrf!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	poodle.sounds()
+	poodle.soundsThreeTimes()
+	fmt.Println(poodle)
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	bark   string
+}
+
+func (d Dog) sounds() {
+	fmt.Println("Dog sounds ", d.bark)
+}
+
+func (d *Dog) soundsThreeTimes() {
+	d.bark = "urf!"
+	d.bark = fmt.Sprintf("%v %v %v", d.bark, d.bark, d.bark)
+	fmt.Println("Dog Barks : ", d.bark)
 }
