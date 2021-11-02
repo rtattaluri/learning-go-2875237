2bd4a236a4f348740f1ef1168b7cd3432ea225fe
rattalur <raviteja_attaluri@bmc.com>
2021-11-02 22:43:30 +0530

    Chnages for function practice

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,44 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	fmt.Println("Functions:")
+	doSomething()
+	total := addValues(1, 3)
+	fmt.Println("values are: ", total)
+
+	total = addMultipleValues(2, 7, 10, 20)
+	fmt.Println("values are: ", total)
+
+	size := 0
+	total, size = addMultipleValuesAndReturnSize(7, 10, 20)
+	fmt.Println("values are: ", total, size)
+
+	a := func() {
+		fmt.Println("Inside anonymized function")
+	}
+	a()
+}
+
+func doSomething() {
+	fmt.Println("Inside do something function")
+}
+
+func addValues(a int, b int) int {
+	return a + b
+}
+
+func addMultipleValues(a ...int) int {
+	total := 0
+	for _, k := range a {
+		total += k
+	}
+	return total
+}
+
+func addMultipleValuesAndReturnSize(a ...int) (int, int) {
+	total := 0
+	for i := range a {
+		total += a[i]
+	}
+	return total, len(a)
 }
