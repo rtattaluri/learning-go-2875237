cd6887edd673064a04669dfd81db8684b82fc3b2
rattalur <raviteja_attaluri@bmc.com>
2021-11-02 23:21:31 +0530

    Advanced calculator task

--- calculator/main.go
+++ calculator/main.go
@@ -9,27 +9,61 @@ import (
 	"strings"
 )
 
-func main() {
+func readInputNumbers() float64 {
 	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Value 1: ")
 	input1, _ := reader.ReadString('\n')
 	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
 	if err != nil {
 		fmt.Println(err)
 		panic("Value 1 must be a number")
 	}
+	return float1
+}
 
+func readInputOperation() string {
+	reader := bufio.NewReader(os.Stdin)
+	input1, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input1)
+}
+
+func main() {
+
+	fmt.Print("Value 1: ")
+	firstInput := readInputNumbers()
 	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
+	secondInput := readInputNumbers()
+	fmt.Print("Enter the operation you would like to perform : + - * /")
+	operation := readInputOperation()
+	var output float64
+	switch operation {
+	case "+":
+		output = sum(firstInput, secondInput)
+	case "-":
+		output = substraction(firstInput, secondInput)
+	case "*":
+		output = multiplication(firstInput, secondInput)
+	case "/":
+		output = Division(firstInput, secondInput)
 	}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+	output = math.Round(output*100) / 100
+	fmt.Printf("Output : %v \n", output)
 
+}
+
+func sum(i, j float64) float64 {
+	return i + j
+}
+
+func substraction(i, j float64) float64 {
+	return i - j
+}
+
+func multiplication(i, j float64) float64 {
+
+	return i * j
+}
+
+func Division(i, j float64) float64 {
+	return i / j
 }
