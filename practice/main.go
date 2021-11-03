43312ae22f8b1545e373457bd43304c7cffb1882
rattalur <raviteja_attaluri@bmc.com>
2021-11-03 15:22:51 +0530

    Changes for parsing json

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -15,7 +17,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Response type: %T\n", resp)
+	//fmt.Printf("Response type: %T\n", resp)
 
 	defer resp.Body.Close()
 
@@ -26,4 +28,32 @@ func main() {
 
 	content := string(bytes)
 	fmt.Print(content)
+	//fmt.Println("Network requests")
+	allTours := toursFromJson(content)
+	fmt.Println("Tour Name", ":", "Tour Price")
+	for _, eachTour := range allTours {
+		fmt.Println(eachTour.Name, ":", eachTour.Price, eachTour.Difficulty)
+	}
+}
+
+type Tour struct {
+	TourId, PackageId, PackageTitle, Name, Blurb, Description, Price, Difficulty, Length, Graphic, Region string
+}
+
+func toursFromJson(jsonString string) []Tour {
+	tours := make([]Tour, 0)
+	jsonDecoder := json.NewDecoder(strings.NewReader(jsonString))
+	_, err := jsonDecoder.Token()
+	if err != nil {
+		panic(err)
+	}
+	var eachTour Tour
+	for jsonDecoder.More() {
+		err = jsonDecoder.Decode(&eachTour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, eachTour)
+	}
+	return tours
 }
