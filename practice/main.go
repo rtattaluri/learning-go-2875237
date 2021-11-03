d61e1240dea5aa1ea106a4983130f1c3902697b3
rattalur <raviteja_attaluri@bmc.com>
2021-11-03 13:17:00 +0530

    Chnages for http requests

--- practice/main.go
+++ practice/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	responseBodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println("Response body", string(responseBodyBytes))
+
+	var jsonStringIntend bytes.Buffer
+	err = json.Indent(&jsonStringIntend, responseBodyBytes, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response body", jsonStringIntend.String())
+	//fmt.Println("Network requests")
 }
